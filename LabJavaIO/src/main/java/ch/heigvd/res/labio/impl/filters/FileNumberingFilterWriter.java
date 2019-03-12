8ae5c9de6270c9fd884a1c9bd81abc842f010dc8
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-03-12 22:13:51 +0100

    Fixed every FileNumberingFilterWritter tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -11,31 +11,75 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
-public class FileNumberingFilterWriter extends FilterWriter {
+public class FileNumberingFilterWriter extends FilterWriter
+{
+  private int testCharAndCounter[] = {1, 0};
+  private char pChar;
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
-  public FileNumberingFilterWriter(Writer out) {
+  public FileNumberingFilterWriter(Writer out)
+  {
     super(out);
   }
 
   @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  public void write(String str, int off, int len) throws IOException
+  {
+    int index = off, indexLimit = off + len;
+    while(index < indexLimit)
+    {
+      write(str.charAt(index));
+      index++;
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    int index = off, indexLimit = off + len;
+    while(index < indexLimit)
+    {
+      write(cbuf[index]);
+      ++index;
+    }
+   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    char character = (char)c;
+    String addT = "";
+
+    if(c != '\n' && pChar == '\r')
+    {
+      testCharAndCounter[1]++;
+      out.write(testCharAndCounter[1] + "\t");
+    }
+
+    if(testCharAndCounter[0] == 0)
+    {
+      out.write(c);
+    }
+    else
+    {
+      testCharAndCounter[0] = 0;
+      testCharAndCounter[1]++;
+      addT = testCharAndCounter[1] + "\t";
+      out.write(addT + character);
+    }
+
+    if(c == '\n')
+    {
+      testCharAndCounter[1]++;
+      addT = testCharAndCounter[1] + "\t";
+      out.write(addT);
+    }
+
+    pChar = character;
+
   }
 
 }
